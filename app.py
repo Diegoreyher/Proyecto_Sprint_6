55c36d36d719f262b92234b977d900f5dcd4e9da
AdowieMa200 <diegoreyher@hotmail.com>
2024-11-05 11:23:21 -0600

    add app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+car_df= pd.read('vehicles_us.csv')
+st.header('Proyecto del Sprint 6')
+hist_button= st.button('Crear Histograma')
+hist_button= st.button('Crear Gráfico de Dispersión')
+
+if hist_button:
+    st.write('Creación de un histograma de anuncios de ventas de autos en EUA')
+
+fig = px.histogram(car_df, x= 'odometer')
+st.plotly_chart(fig, use_container_width=True)
+
+##Sustituir los botones por casillas de verificación en streamlit
+#st.checkbox()
+
+
