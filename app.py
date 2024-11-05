274bd81067d2c26da63ff873812fcfd1dc7d5247
AdowieMa200 <diegoreyher@hotmail.com>
2024-11-05 11:38:46 -0600

    add app.py

--- app.py
+++ app.py
@@ -5,13 +5,20 @@ import plotly.express as px
 car_df= pd.read_csv('vehicles_us.csv')
 st.header('Proyecto del Sprint 6')
 hist_button= st.button('Crear Histograma')
-hist_button= st.button('Crear Gráfico de Dispersión')
+disp_button= st.button('Crear Gráfico de Dispersión')
 
 if hist_button:
     st.write('Creación de un histograma de anuncios de ventas de autos en EUA')
+    fig = px.histogram(car_df, x= 'odometer')
+    st.plotly_chart(fig, use_container_width=True)
 
-fig = px.histogram(car_df, x= 'odometer')
-st.plotly_chart(fig, use_container_width=True)
+if disp_button:
+    st.write('Creación de un gráfico de dispersión de anuncios de ventas de autos en EUA')
+    fig = px.scatter(car_df, x= 'odometer')
+    st.plotly_chart(fig, use_container_width=True)
+
+
+
 
 ##Sustituir los botones por casillas de verificación en streamlit
 #st.checkbox()
