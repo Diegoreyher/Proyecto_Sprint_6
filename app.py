8b751e91e149e336e535fff36be3835bbf5a8771
AdowieMa200 <diegoreyher@hotmail.com>
2024-11-05 16:07:36 -0600

    add app.py

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ car_df= pd.read_csv('vehicles_us.csv')
 st.header('Proyecto del Sprint 6')
 hist_button= st.button('Crear Histograma')
 disp_button= st.button('Crear Gráfico de Dispersión')
+hist_pg_button= st.checkbox('Crear Histograma en una página nueva')
+disp_pg_button= st.checkbox('Crear Gráfico de Dispersión en una página nueva')
+
 
 if hist_button:
     st.write('Creación de un histograma de anuncios de ventas de autos en EUA')
@@ -14,9 +17,18 @@ if hist_button:
 
 if disp_button:
     st.write('Creación de un gráfico de dispersión de anuncios de ventas de autos en EUA')
-    fig = px.scatter(car_df, x="odometer", y="price") # crear un gráfico de dispersión
+    fig = px.scatter(car_df, x="odometer", y="price") 
     st.plotly_chart(fig, use_container_width=True)
 
+if disp_pg_button:
+    st.write('Redirigiendo...')
+    fig1 = px.scatter(car_data, x="odometer", y="price")
+    fig1.show() # crear gráfico de dispersión
+
+if hist_pg_button:
+    st.write('Redirigiendo...')
+    fig2 = px.histogram(car_data, x="odometer") 
+    fig2.show() 
 
 
 ##Sustituir los botones por casillas de verificación en streamlit
